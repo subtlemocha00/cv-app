edab815f748b0ba2bd87fad15a76b3866006cda4
subtlemocha00 <kturley@gmail.com>
2023-11-29 00:01:01 -0500

    reduced TitleBlock to just a div

--- src/TitleBlock.jsx
+++ src/TitleBlock.jsx
@@ -1,12 +1,3 @@
-import { v4 as uuid4 } from "uuid";
-
-const users = [
-	{ name: "Kevin Turley", id: uuid4() },
-	{ name: "Kati Chivers", id: uuid4() },
-	{ name: "Henry Chivers Turley", id: uuid4() },
-	{ name: "Zappa", id: uuid4() },
-];
-
 export default function TitleBlock() {
 	return (
 		<div className="text-center m-0 p-3 bg-info text-white">
