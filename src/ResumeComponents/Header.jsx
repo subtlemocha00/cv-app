3afbbf692460b417ec746a343dc55cb36f547977
subtlemocha00 <kturley@gmail.com>
2023-12-22 00:25:26 -0500

    adjusted prop handling

--- src/ResumeComponents/Header.jsx
+++ src/ResumeComponents/Header.jsx
@@ -3,32 +3,30 @@ import PropTypes from "prop-types";
 export default function Header({ personalInfo }) {
 	// validates the prop types
 	Header.propTypes = {
-		personalInfo: PropTypes.arrayOf(
-			PropTypes.shape({
-				userName: PropTypes.string,
-				userEmail: PropTypes.string,
-				userPhone: PropTypes.string,
-				userLocation: PropTypes.string,
-			})
-		),
+		personalInfo: PropTypes.shape({
+			userName: PropTypes.string,
+			userEmail: PropTypes.string,
+			userPhone: PropTypes.string,
+			userLocation: PropTypes.string,
+		}),
 	};
-	const info = personalInfo[0];
+	console.log("personal info HEADER: ", personalInfo);
 	return (
 		<section className="container p-0">
 			<div className="row m-0 p-2 col col-12">
-				<h1 id="name">{info.userName}</h1>
+				<h1 id="name">{personalInfo.userName}</h1>
 				<div className="p-1 d-flex justify-content-around">
 					<h6>
 						<i className="fas fa-envelope"></i>{" "}
-						<span id="email">{info.userEmail}</span>
+						<span id="email">{personalInfo.userEmail}</span>
 					</h6>
 					<h6>
 						<i className="fas fa-phone"></i>{" "}
-						<span id="phone">{info.userPhone}</span>
+						<span id="phone">{personalInfo.userPhone}</span>
 					</h6>
 					<h6>
 						<i className="fas fa-map-marker"></i>{" "}
-						<span id="location">{info.userLocation}</span>
+						<span id="location">{personalInfo.userLocation}</span>
 					</h6>
 				</div>
 			</div>
