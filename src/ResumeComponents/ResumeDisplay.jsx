8ba34b568922f197f4e66c8cdaea5bdb83e9e334
subtlemocha00 <kturley@gmail.com>
2023-12-22 00:25:39 -0500

    added state management

--- src/ResumeComponents/ResumeDisplay.jsx
+++ src/ResumeComponents/ResumeDisplay.jsx
@@ -3,61 +3,16 @@ import Header from "./Header";
 import EmploymentContent from "./EmploymentContent";
 import EducationContent from "./EducationContent";
 import "./ResumeDisplay.css";
+import defaultData from "../default-data";
+import { useState } from "react";
 
-export default function ResumeDisplay({
-	personalInfo,
-	employmentInfo,
-	educationInfo,
-}) {
-	// validates the prop types
-	ResumeDisplay.propTypes = {
-		personalInfo: PropTypes.arrayOf(
-			PropTypes.shape({
-				userName: PropTypes.string,
-				email: PropTypes.string,
-				phone: PropTypes.string,
-				userLocation: PropTypes.string,
-			})
-		),
-		employmentInfo: PropTypes.arrayOf(
-			PropTypes.shape({
-				company: PropTypes.string,
-				title: PropTypes.string,
-				city: PropTypes.string,
-				country: PropTypes.string,
-				startDate: PropTypes.shape({
-					month: PropTypes.string,
-					year: PropTypes.string,
-				}),
-				endDate: PropTypes.shape({
-					month: PropTypes.string,
-					year: PropTypes.string,
-				}),
-				skills: PropTypes.arrayOf(PropTypes.string),
-			})
-		),
-		educationInfo: PropTypes.arrayOf(
-			PropTypes.shape({
-				university: PropTypes.string,
-				title: PropTypes.string,
-				city: PropTypes.string,
-				country: PropTypes.string,
-				startDate: PropTypes.shape({
-					month: PropTypes.string,
-					year: PropTypes.string,
-				}),
-				endDate: PropTypes.shape({
-					month: PropTypes.string,
-					year: PropTypes.string,
-				}),
-				skills: PropTypes.arrayOf(PropTypes.string),
-			})
-		),
-	};
-	// const [employmentInfo, setEmploymentInfo] = useState({});
+export default function ResumeDisplay() {
+	const [userInfo, setUserInfo] = useState(defaultData);
+	const { personalInfo, educationInfo, employmentInfo } = userInfo;
+	// console.log("Personal Info: ", personalInfo);
 	return (
 		<div className="d-flex flex-column w-75 ms-3">
-			<Header personalInfo={personalInfo} />
+			<Header personalInfo={personalInfo} onChange={setUserInfo} />
 			<EmploymentContent employmentInfo={employmentInfo} />
 			<EducationContent educationInfo={educationInfo} />
 		</div>
